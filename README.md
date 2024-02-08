ff0a1e754bcf0943f237a273a18f7271f8d6ce7d
angolosFlame <couragetkordzi@gmail.com>
2024-02-08 11:19:23 -0800

    My weather app

---
+++ README.md
@@ -0,0 +1,35 @@
+In this project I built a console-based weather application that fetches data from an API and displays weather information 
+for a given location in C to be run in Ubuntu 20.04 and uses the Betty code style.
+
+Building a console-based weather application in C involves making HTTP requests to weather API and parsing the JSON response to display relevant information.
+
+For simplicity, I'll use the OpenWeatherMap API for this project. I signed up for a free API key from OpenWeatherMap.
+
+Firstly, l needed to install the libcurl library to make HTTP requests. In Ubuntu 20.04, using the code below;
+
+sudo apt-get update
+sudo apt-get install libcurl4-openssl-dev
+
+Challenges
+
+I had challenges with the Betty linter approving the Betty code styling which I am working on the errors found.
+I had some indentation errors which is being fixed
+API generation was seamless however, i hit a snag with the url which is currently resolved with some meticulous research
+I had a compilation challenge because of URL definition issues. 
+An https url contains several characters that make she sees it as a character for expansion which tends to impede the exact meaning of the url string. 
+
+
+Collaboration/Communication
+
+Before I jumped on this final project, I had a discussion with a peer about a list of projects I would like to try my hands on. 
+However he indicated that I had powerful ideas but because of the limited time to execute the project task I should focus on something lighter such as building 
+with a few features and then add more afterwards. I was happy and took his advice in whole. 
+Additionally he was excited about the task at hand. He said “For sure, I feel like I'll only start calling myself a software developer after putting some products out there”. 
+
+
+Project Updates
+
+My code is in the testing phase. I want to make sure I am able to display the weather for my city and then invite my peers to try it out for their cities as well. 
+Furthermore I will push my code into my github account.
+I successfully ran my Betty linter after correcting a few indentations regarding my loops.
+
---
+++ weather_app.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+
+/* This is where the Stormglass' actual OpenWeatherMap API key goes */
+#define API_KEY "f511eca8-c0f8-11ee-84d9-0242ac130002-f511ed16-c0f8-11ee-84d9-0242ac130002"
+#define API_URL "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s"
+
+/* Structure to hold the HTTP response data */
+typedef struct {
+    char *data;
+    size_t size;
+} MemoryStruct;
+
+/* Callback function to handle writing HTTP response data */
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) 
+{
+	size_t realsize = size * nmemb;
+	MemoryStruct *mem = (MemoryStruct *)userp;
+	
+	mem->data = realloc(mem->data, mem->size + realsize + 1);
+	if (mem->data == NULL) 
+	{
+		fprintf(stderr, "Memory allocation error\n");
+		return (0);
+	}
+	
+	memcpy(&(mem->data[mem->size]), contents, realsize);
+	mem->size += realsize;
+	mem->data[mem->size] = '\0';
+	
+	return (realsize);
+}
+
+/* Function to get weather information for a given city */
+void get_weather(char *city) 
+{
+	CURL *curl;
+	CURLcode res;
+	
+	MemoryStruct chunk;
+	chunk.data = malloc(1);
+	chunk.size = 0;
+	
+	/* Build the API URL with the city and API key */
+	char url[256];
+	sprintf(url, API_URL, city, API_KEY);
+	
+	/* Initialize the libcurl library */
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+	
+	if (curl) 
+	{
+		/* Set the URL to fetch */
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		
+		/* Set the callback function to handle response data */
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+		
+		/* Perform the HTTP request */
+		res = curl_easy_perform(curl);
+		
+		/* Check for errors during the HTTP request */
+		if (res != CURLE_OK) 
+		{
+			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+		} 
+		else 
+		{
+			/* Print the weather information for the given city */
+			printf("Weather information for %s:\n%s\n", city, chunk.data);
+		}
+		
+		/* Cleanup resources */
+		curl_easy_cleanup(curl);
+		free(chunk.data);
+	}
+	
+	/* Cleanup global libcurl resources */
+	curl_global_cleanup();
+}
+
+int main(int argc, char *argv[]) 
+{
+	/* Check if the correct number of command-line arguments is provided */
+	if (argc != 2) 
+	{
+		fprintf(stderr, "Usage: %s <city>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	
+	/* Call the function to get weather information for the specified city */
+	get_weather(argv[1]);
+	
+	return (EXIT_SUCCESS);
+}
